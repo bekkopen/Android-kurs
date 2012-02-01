3d65b5cb64e2afb021c8beb412eb88f9991ac34c
Thomas Johan Eggum <thojeggu@gmail.com>
2012-02-01 13:02:38 +0100

    added examples for second step in presentation

--- HelloTranslate/src/no/bekk/open/androidkurs/hellotranslate/HelloTranslateActivity.java
+++ HelloTranslate/src/no/bekk/open/androidkurs/hellotranslate/HelloTranslateActivity.java
@@ -2,12 +2,32 @@ package no.bekk.open.androidkurs.hellotranslate;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HelloTranslateActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
+    
+    
+	
+	
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
+        
+        Button translateButton = (Button)findViewById(R.id.translatebutton);
+        
+        translateButton.setOnClickListener(
+        		new OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						TextView translatdText = (TextView)findViewById(R.id.translatedText);
+						translatdText.setText("Hello again");
+					}
+				}
+        );
+	}
 }
