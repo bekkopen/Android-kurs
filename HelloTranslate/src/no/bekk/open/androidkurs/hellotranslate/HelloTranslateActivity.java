11f85e94f0f4505207c6a57995d08c67b1f4d00a
Thomas Johan Eggum <thojeggu@gmail.com>
2012-02-01 16:41:22 +0100

    remove some unused imports.

--- HelloTranslate/src/no/bekk/open/androidkurs/hellotranslate/HelloTranslateActivity.java
+++ HelloTranslate/src/no/bekk/open/androidkurs/hellotranslate/HelloTranslateActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TableLayout;
 
 public class HelloTranslateActivity extends Activity {
 
